eff2b357f8b8ff1b2cd63aa82677e6ea296ebad8
Ryoya Takayama <takayamar827@gmail.com>
2020-10-31 12:16:56 +0900

    add: datatables追加

--- src/resources/ts/dashboard/dashboard.tsx
+++ src/resources/ts/dashboard/dashboard.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { User } from '../layouts/app';
+import Datatables from '../modules/datatables';
 
 const Dashboard: React.FC = () => {
     const user = useContext(User);
@@ -11,10 +12,20 @@ const Dashboard: React.FC = () => {
     return (
         <>
             <div className="top">
-                <div className="mystatus"></div>
+                <div className="mystatus">
+                    <div className="mystatus_head">〇〇さんのTodo状況</div>
+                    <ul className="mystatus_status">
+                        <li>進行中〇〇件</li>
+                        <li>完了〇〇件</li>
+                        <li>期限切れ〇〇件</li>
+                    </ul>
+                </div>
                 <div className="linklist"></div>
             </div>
-            <div className="bottom"></div>
+            <div className="bottom">
+                <div className="contents_title"></div>
+                <Datatables />
+            </div>
         </>
     );
 };
