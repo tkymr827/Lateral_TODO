d35292eff535426f9607ed48df461694980a286c
Ryoya Takayama <takayamar827@gmail.com>
2020-11-12 00:50:45 +0900

    del: 不要個所の削除

--- src/resources/ts/list/list.tsx
+++ src/resources/ts/list/list.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { MDBDataTableV5, MDBInput } from 'mdbreact';
+import { MDBDataTableV5 } from 'mdbreact';
 import { Button, Form } from 'react-bootstrap';
 import axios from 'axios';
 import DetaillModal from '../modules/detail_modal';
@@ -8,7 +8,6 @@ const List: React.FC = () => {
     const [modalShow, setModalShow] = useState(false);
     const [modalData, setModalData] = useState([]);
     const [selectDelete, setSelectDelete] = useState([] as any);
-    // const [selectDelete, setSelectDelete] = useState([]);
 
     const [datatable, setDatatable] = useState({
         columns: [
@@ -67,14 +66,11 @@ const List: React.FC = () => {
     useEffect(() => {
         const getTodos = async () => {
             try {
-                // const response = await axios.get('/api/get_todos', { params: { mytodo: false } });
                 const response = await axios.get('/api/get_todos');
                 console.log(response.data);
                 const rows = response.data.map((todo: { [key: string]: string }) => ({
                     id: todo.id,
-                    // checkbox: <MDBInput label="" type="checkbox" onClick={toggleCheck} />,
                     checkbox: <Form.Check value={todo.id} onClick={() => toggleCheck(todo.id)} />,
-                    // checkbox: <Form.Check value={todo.id} onClick={toggleCheck} />,
                     task_name: todo.task_name,
                     user_name: todo.user_name,
                     release: todo.release,
@@ -83,11 +79,7 @@ const List: React.FC = () => {
                     achievement_date: todo.achievement_date,
                     content: todo.content,
                     editor: todo.editor,
-                    // clickEvent: () => setModalShow(true),
-                    // button: <Button variant="info" onClick={() => setModalShow(true)} />,
                     button: (
-                        // <Button variant="info" onClick={() => setModalShow(true)}>
-                        // <Button variant="info" onClick={() => handleClick(todo.id)}>
                         <Button variant="info" onClick={() => handleClick(rows, todo.id)}>
                             詳細
                         </Button>
@@ -101,90 +93,19 @@ const List: React.FC = () => {
         };
         getTodos();
     }, []);
-    // console.log('--check-');
-    // console.log(datatable.rows);
-    // console.log('---');
 
     let state: any = [];
 
     const toggleCheck = (id: any) => {
-        console.log(id);
-        // console.log(e.currentTarget.value);
-        // console.log(typeof id);
-        // console.log(id.currentTarget.value);
-        // const select_id: any = id.currentTarget.value;
-
-        // if (selectDelete.includes(select_id)) {
-        // if (selectDelete.includes(select_id)) {
-        //     console.log('既に入ってるよ');
-        //     setSelectDelete(selectDelete.filter((value: any) => value !== select_id));
-        // } else {
-        //     console.log('なかったよ');
-        //     setSelectDelete([...selectDelete, select_id]);
-        // }
-
-        // console.log(selectDelete.includes(id));
-        // console.log(selectDelete.includes(1));
-
-        // if (selectDelete.includes(id.currentTarget.value)) {
-        //     // setSelectDelete('haitteruyo');
-        //     // setSelectDelete(selectDelete.filter((value: any) => value !== id));
-        //     // setSelectDelete((state:any) => [...state.filter(state) => state !==id])
-        //     setSelectDelete(selectDelete.filter((item: any) => item !== id.currentTarget.value));
-        // } else {
-        //     // setSelectDelete([...selectDelete, id]);
-        //     // setSelectDelete((state: any) => ({ ...state, id }));
-        //     // setSelectDelete([id]);
-        //     // setSelectDelete(id);
-        //     // setSelectDelete('naiyo');
-        //     // setSelectDelete(id);
-        //     setSelectDelete((state: any) => [...state, id.currentTarget.value]);
-        // }
-
-        // let state;
-        // if (selectDelete.includes(e.currentTarget.value)) {
-        // let state: any = selectDelete;
-        // let state: any = [];
-        // state = selectDelete;
-        // if (selectDelete.includes(id)) {
         if (state.includes(id)) {
-            console.log('入ってるよ');
-            // state = selectDelete.filter((item: any) => item !== id);
             state = state.filter((item: any) => item !== id);
         } else {
-            console.log('入ってないよ');
-            console.log(state);
-            // console.log(id);
-            // console.log(selectDelete);
-            // setSelectDelete((state: any) => [...state, id]);
-            // state = [...selectDelete, id];
-            // state.push(...selectDelete, id);
             state.push(id);
-            // setSelectDelete([...selectDelete, id]);
         }
-
-        console.log(state);
         setSelectDelete(state);
     };
 
     const handleClick = (rows: any, id: string) => {
-        // console.log(rows);
-        // console.log(datatable.rows.map((data: { [key: string]: string }) => data.id == id));
-        // console.log(datatable.rows.find((data: { [key: string]: string }) => data.id == id));
-
-        // console.log(datatable.rows.find((e: any) => e.id == id));
-        // console.log(rows.find((todo: { [key: string]: string }) => todo.id === id));
-        // const hoge = datatable.rows.find(
-        //     (todo: { [key: string]: string }) => todo.user_name == 'test'
-        // );
-        // const hoge = datatable.rows.find((e: any) => e.id == id);
-        // console.log(hoge);
-
-        // console.log('genmitu');
-
-        // console.log(datatable.rows.find(data => data.id == id));
-        // console.log(dessertList.find(dessert => dessert.name == 'チョコレート'))
-        // setModalData(datatable)
         setModalData(rows.find((todo: { [key: string]: string }) => todo.id === id));
         setModalShow(true);
     };
--- src/resources/ts/modules/detail_modal.tsx
+++ src/resources/ts/modules/detail_modal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { Button, Modal, Container, Row, Col, Form, FormGroup } from 'react-bootstrap';
 import axios from 'axios';
 import { User } from '../layouts/app';
@@ -22,31 +22,7 @@ const DetailModal: React.FC<Props> = props => {
     const user = useContext(User);
     const [isEdit, setIsEdit] = useState(false);
     const [form_value, setFormValue] = useState(initialState);
-    // const [form_value, setFormValue] = useState({
-    //     id: props.data.id,
-    //     task_name: '',
-    //     editor: user.name,
-    //     release: '',
-    //     progress: '',
-    //     complete_date: '',
-    //     achievement_date: '',
-    //     content: '',
-    // });
-    // const [achievement_date, setAchievementDate] = useState(props.data.achievement_date);
-    // const [complete_date, setCompleteDate] = useState(props.data.complete_date);
-    // const id = props.data.id;
-    // console.log(`idは:${props.data.id}`);
-    // console.log('--props--');
-    // console.log(props);
-    // console.log('---');
 
-    // console.log('--form_value--');
-    // console.log(form_value);
-    // console.log('---');
-
-    // useEffect(() => {
-    //     setFormValue(state => ({ ...state, id: id, editor: user.name }));
-    // }, []);
     console.log('-form_value-sotogawa');
     console.log(form_value);
     console.log('------------');
@@ -62,22 +38,6 @@ const DetailModal: React.FC<Props> = props => {
     };
 
     const editTodo = (e: any) => {
-        console.log('');
-        // e.preventDefault();
-        console.log('edittodo username');
-        console.log(user.name);
-        console.log(props.data.id);
-        // console.log(id);
-        console.log('---');
-        // setFormValue(state => ({
-        //     ...state,
-        //     // id: props.data.id,
-        //     id: id,
-        //     editor: user.name,
-        //     // complete_date: complete_date,
-        //     // achievement_date: achievement_date,
-        // }));
-
         try {
             const response = axios.post('/api/edit_todos', {
                 id: props.data.id,
@@ -88,48 +48,16 @@ const DetailModal: React.FC<Props> = props => {
             console.log(error);
         }
 
-        console.log('--edittodo--');
-        console.log(form_value);
-        console.log('----');
         e.preventDefault();
     };
 
     const changeFormValue = (e: any) => {
-        // console.log(e.currentTarget);
-        // console.log(e.currentTarget.value);
-        // console.log(e.currentTarget.id);
-        // e.persist();
-        // e.persist();
         const value = e.currentTarget.value;
         const key = e.currentTarget.id;
 
-        // setFormValue(state => ({ ...state, [e.currentTarget.id]: hoge }));
         setFormValue(state => ({ ...state, [key]: value }));
-        // console.log(form_value);
-
-        // setFormValue(state => ({ ...state, [e.currentTarget.id]: e.currentTarget.value }));
     };
 
-    // const changeTaskName = (e: any) => {
-    // const test = (e: any) => {
-    //     console.log(e.currentTarget.value);
-    //     let hoge = e.currentTarget.value;
-    //     // setFormValue(state => ({ ...state, task_name: e.currentTarget.value }));
-    //     setFormValue(state => ({ ...state, task_name: hoge }));
-    //     console.log(form_value);
-    // };
-
-    // const changeAchievementDate = (date: any) => {
-    //     setAchievementDate(date.target.value);
-    //     const value = date.currentTarget.value;
-    //     setFormValue(state => ({ ...state, achievement_date: value }));
-    // };
-    // const changeCompleteDate = (date: any) => {
-    //     setCompleteDate(date.target.value);
-    //     const value = date.currentTarget.value;
-
-    //     setFormValue(state => ({ ...state, complete_date: value }));
-    // };
     const handleEdit = () => {
         setIsEdit(state => !state);
         setFormValue(state => ({
@@ -154,13 +82,11 @@ const DetailModal: React.FC<Props> = props => {
                     backdrop="static"
                 >
                     <Form onSubmit={editTodo}>
-                        {/* <Modal.Header closeButton> */}
                         <Modal.Header>
                             <Modal.Title id="contained-modal-title-vcenter">
                                 <Form.Group controlId="task_name">
                                     <Form.Label>タスク名</Form.Label>
                                     <Form.Control
-                                        // defaultValue={props.data.task_name}
                                         defaultValue={form_value.task_name}
                                         onChange={changeFormValue}
                                     />
@@ -174,7 +100,6 @@ const DetailModal: React.FC<Props> = props => {
                                         <Form.Label>タスク内容</Form.Label>
                                         <Form.Control
                                             as="textarea"
-                                            // defaultValue={props.data.content}
                                             defaultValue={form_value.content}
                                             onChange={changeFormValue}
                                         />
@@ -186,56 +111,42 @@ const DetailModal: React.FC<Props> = props => {
                                 </Form.Row>
                                 <Form.Row className="tododetail_unit">
                                     <Form.Group as={Col} controlId="release">
-                                        {/* <Col md={6}> */}
                                         <Form.Label>公開範囲</Form.Label>
                                         <Form.Control as="select" onChange={changeFormValue}>
                                             <option hidden>選択</option>
                                             <option value="0">Public</option>
                                             <option value="1">Private</option>
                                         </Form.Control>
-                                        {/* </Col> */}
                                     </Form.Group>
                                     <Form.Group as={Col} controlId="progress">
-                                        {/* <Col md={6}> */}
                                         <Form.Label>進行度</Form.Label>
                                         <Form.Control as="select" onChange={changeFormValue}>
                                             <option hidden>選択</option>
                                             <option value="進行中">進行中</option>
                                             <option value="完了">完了</option>
                                         </Form.Control>
-                                        {/* </Col> */}
                                     </Form.Group>
                                 </Form.Row>
                                 <Form.Row className="tododetail_unit">
                                     <Form.Group as={Col} controlId="achievement_date">
-                                        {/* <Col md={6}>達成日:{props.data.achievement_date}</Col> */}
-                                        {/* <Col md={6}> */}
                                         <Form.Label>達成日</Form.Label>
                                         <input
                                             type="date"
                                             name="achievement_date"
                                             id="achievement_date"
                                             value={form_value.achievement_date}
-                                            // value={achievement_date}
-                                            // onChange={() => changeAchievementDate}
                                             onChange={changeFormValue}
                                         />
-                                        {/* </Col> */}
                                     </Form.Group>
                                     <Form.Group as={Col} controlId="complete_date">
-                                        {/* <Col md={6}> */}
                                         <Form.Label>完了日</Form.Label>
                                         <input
                                             type="date"
                                             name="complete_date"
                                             id="complete_date"
                                             value={form_value.complete_date}
-                                            // value={complete_date}
-                                            // onChange={() => changeCompleteDate}
                                             onChange={changeFormValue}
                                         />
-                                        {/* </Col> */}
-                                        {/* <Col md={6}>完了日:{props.data.complete_date}</Col> */}
                                     </Form.Group>
                                 </Form.Row>
                             </Container>
@@ -248,13 +159,6 @@ const DetailModal: React.FC<Props> = props => {
                             >
                                 戻る
                             </Button>
-                            {/* <Button
-                                className="modal_btn"
-                                variant="danger"
-                                onClick={() => delTodo()}
-                            >
-                                削除
-                            </Button> */}
                             <Button className="modal_btn" type="submit" variant="primary">
                                 編集完了
                             </Button>
@@ -303,12 +207,7 @@ const DetailModal: React.FC<Props> = props => {
                         <Button className="modal_btn" variant="danger" onClick={() => delTodo()}>
                             削除
                         </Button>
-                        <Button
-                            className="modal_btn"
-                            variant="primary"
-                            // onClick={() => setIsEdit(state => !state)}
-                            onClick={handleEdit}
-                        >
+                        <Button className="modal_btn" variant="primary" onClick={handleEdit}>
                             編集
                         </Button>
                     </Modal.Footer>
