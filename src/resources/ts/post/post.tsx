1c609971c95ebbc932307b6812eb6321e5103164
Ryoya Takayama <takayamar827@gmail.com>
2020-11-12 22:00:10 +0900

    add: stateに値が入るように

--- src/resources/ts/post/post.tsx
+++ src/resources/ts/post/post.tsx
@@ -2,37 +2,56 @@ import React, { useState } from 'react';
 import { Button, Container, Col, Form } from 'react-bootstrap';
 import axios from 'axios';
 
+const initialState = {
+    task_name: '',
+    release: '',
+    progress: '',
+    complete_date: '',
+    achievement_date: '',
+    content: '',
+};
+
 const Post: React.FC = () => {
+    const [post_value, setPostValue] = useState(initialState);
+
+    const changePostValue = (e: any) => {
+        const value = e.currentTarget.value;
+        const key = e.currentTarget.id;
+
+        setPostValue(state => ({ ...state, [key]: value }));
+        console.log(post_value);
+    };
+
     return (
         <div className="post">
             <Form className="post_form">
                 <Container>
                     <Form.Row>
-                        <Form.Group as={Col}>
+                        <Form.Group as={Col} controlId="task_name">
                             <Form.Label>タスク名</Form.Label>
-                            <Form.Control />
+                            <Form.Control onChange={changePostValue} />
                         </Form.Group>
                     </Form.Row>
 
                     <Form.Row>
-                        <Form.Group as={Col} className="post_form_content">
+                        <Form.Group as={Col} className="post_form_content" controlId="content">
                             <Form.Label>タスク内容</Form.Label>
-                            <Form.Control as="textarea" />
+                            <Form.Control as="textarea" onChange={changePostValue} />
                         </Form.Group>
                     </Form.Row>
 
                     <Form.Row>
-                        <Form.Group as={Col}>
+                        <Form.Group as={Col} controlId="release">
                             <Form.Label>公開範囲</Form.Label>
-                            <Form.Control as="select">
+                            <Form.Control as="select" onChange={changePostValue}>
                                 <option hidden>選択</option>
                                 <option value="0">Public</option>
                                 <option value="1">Private</option>
                             </Form.Control>
                         </Form.Group>
-                        <Form.Group as={Col}>
+                        <Form.Group as={Col} controlId="progress">
                             <Form.Label>進行度</Form.Label>
-                            <Form.Control as="select">
+                            <Form.Control as="select" onChange={changePostValue}>
                                 <option hidden>選択</option>
                                 <option value="進行中">進行中</option>
                                 <option value="完了">完了</option>
@@ -40,10 +59,10 @@ const Post: React.FC = () => {
                         </Form.Group>
                     </Form.Row>
 
-                    <Form.Row className="tododetail_unit">
-                        <Form.Group as={Col}>
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="achievement_date">
                             <Form.Label>達成日</Form.Label>
-                            <Form.Control type="date" value="2020-11-12"></Form.Control>
+                            <Form.Control type="date" onChange={changePostValue}></Form.Control>
 
                             {/* <input
                             type="date"
@@ -53,9 +72,9 @@ const Post: React.FC = () => {
                             // onChange={changeFormValue}
                         /> */}
                         </Form.Group>
-                        <Form.Group as={Col}>
+                        <Form.Group as={Col} controlId="complete_date">
                             <Form.Label>完了日</Form.Label>
-                            <Form.Control type="date" value="2020-01-01"></Form.Control>
+                            <Form.Control type="date" onChange={changePostValue}></Form.Control>
                         </Form.Group>
                         {/* <Form.Group as={Col} controlId="complete_date">
                         <Form.Label>完了日</Form.Label>
